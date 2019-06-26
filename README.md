268ea21810c979a45890170dc8fbba39be109d9a
nirumasa <maxzateam@gmail.com>
2019-06-26 16:45:28 +0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # youtube-dl-sh-dev-script
+
+Need
+  - Youtube-dl
+  - ffmpeg
+
+  
+Intallation
+  Place youtube_marge.sh on folder
+  then  chmod a+x youtube_marge.sh
+  run	./youtube_marge.sh
+  input Youtube Video ID 
+  Simple Video id 6********k
+  
+Note Video type Support 137+140
---
+++ youtube_marge.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo VideoID?
+read video_id
+mkdir $video_id
+cd $video_id
+youtube-dl -f 137 $video_id
+youtube-dl -f 140 $video_id
+
+
+echo "XXXXXXXXXXXXX  FILE OUT XXXXXXXXXXXXXXXX"
+for f in *.mp4; do
+video_f_name=$f
+    #printf '%s.mp4\n' "${f%.mp4}"
+done
+
+for f in *.m4a; do
+audio_f_name=$f
+    #printf '%s.m4a\n' "${f%.m4a}"
+done
+
+
+echo $video_f_name
+echo $audio_f_name 
+
+echo ">>FFMPEG FILE Mix"
+
+mkdir out
+ffmpeg -i "$video_f_name" -i "$audio_f_name"  -c:v copy -c:a aac -strict experimental "out/$video_f_name"
+
+echo "Completed!"
+
+echo "Cleaning Resource"
+rm "$video_f_name"
+rm "$audio_f_name"
+
+
+echo "Copy File Out To Top Folder"
+cp "out/$video_f_name" "$video_f_name" 
+
+rm -rf "out"
+
+echo "Bye Bye!";
